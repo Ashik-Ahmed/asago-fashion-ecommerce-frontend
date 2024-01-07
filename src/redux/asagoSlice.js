d9bb9c31983b9798c4b50e31169f24fd473bed92
ashik-ahmed <ashikahmed121@gmail.com>
2024-01-07 22:01:46 +0600

    redux slice file created

---
+++ src/redux/asagoSlice.js
@@ -0,0 +1,55 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    productData: [],
+};
+
+export const asagoSlice = createSlice({
+    name: "asago",
+    initialState,
+    reducers: {
+        addToCart: (state, action) => {
+            const existingProduct = state?.productData.find((item) => {
+                item?._id === action?.payload?._id;
+            });
+            if (existingProduct) {
+                existingProduct.quantity += action.payload.quantity;
+            } else {
+                state.productData.push(action.payload);
+            }
+        },
+        increaseQuantity: (state, action) => {
+            const existingProduct = state.productData.find(
+                (item) => item._id === action.payload._id
+            );
+            existingProduct && existingProduct.quantity++;
+        },
+        decreaseQuantity: (state, action) => {
+            const existingProduct = state.productData.find(
+                (item) => item._id === action.payload._id
+            );
+            if (existingProduct?.quantity === 1) {
+                existingProduct.quantity === 1;
+            } else {
+                existingProduct && existingProduct.quantity--;
+            }
+        },
+        deleteProduct: (state, action) => {
+            state.productData = state.productData.filter(
+                (item) => item._id !== action.payload
+            );
+        },
+        resetCart: (state) => {
+            state.productData = [];
+        },
+    },
+});
+
+export const {
+    addToCart,
+    increaseQuantity,
+    decreaseQuantity,
+    deleteProduct,
+    resetCart,
+} = asagoSlice.actions;
+export default asagoSlice.reducer;
