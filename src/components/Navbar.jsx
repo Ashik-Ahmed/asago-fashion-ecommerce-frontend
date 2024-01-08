5f918af9478af5a66ca3ad2ae6cc19c830d0fec7
ashik-ahmed <ashikahmed121@gmail.com>
2024-01-08 22:09:08 +0600

    side navbar added for mobile devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,18 @@ import Link from "next/link";
 import logo from "@/app/assets/logo.jpeg";
 import { FaSearch } from "react-icons/fa";
 import { IoCloseOutline } from "react-icons/io5";
+import { motion } from "framer-motion";
 import { HiMenuAlt2 } from "react-icons/hi";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+import { MdClose } from "react-icons/md";
 
 const Navbar = () => {
     const pathname = usePathname();
     const [searchQuery, setSearchQuery] = useState("");
+    const [sidenav, setSidenav] = useState(false);
+    const [category, setCategory] = useState(false);
+    const [brand, setBrand] = useState(false);
 
     const navBarList = [
         {
@@ -65,7 +70,94 @@ const Navbar = () => {
                         </Link>
                     ))}
                 </div>
-                <HiMenuAlt2 className="inline-flex md:hidden cursor-pointer w-8 h-6" />
+                <HiMenuAlt2 onClick={() => setSidenav(!sidenav)} className="inline-flex md:hidden cursor-pointer w-8 h-6" />
+
+                {sidenav && (
+                    <div className="fixed top-0 left-0 w-full h-screen bg-black text-gray-200 bg-opacity-80 z-50">
+                        <motion.div
+                            initial={{ x: -300, opacity: 0 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            transition={{ duration: 0.5 }}
+                            className="w-[80%] h-full relative"
+                        >
+                            <div className="w-full h-full bg-primeColor p-6">
+                                <Image
+                                    className="w-28 mb-6"
+                                    src={logo}
+                                    alt="logoLight"
+                                />
+                                <ul className="text-gray-200 flex flex-col gap-2">
+                                    {navBarList.map((item) => (
+                                        <li
+                                            className="font-normal hover:font-bold items-center text-lg text-gray-200 hover:underline underline-offset-[4px] decoration-[1px] hover:text-white md:border-r-[2px] border-r-gray-300 hoverEffect last:border-r-0"
+                                            key={item._id}
+                                        >
+                                            <Link
+                                                href={item.link}
+                                                // state={{ data: location.split("/")[1] }}
+                                                onClick={() => setSidenav(false)}
+                                            >
+                                                {item.title}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                                <div className="mt-4">
+                                    <h1
+                                        onClick={() => setCategory(!category)}
+                                        className="flex justify-between text-base cursor-pointer items-center font-titleFont mb-2"
+                                    >
+                                        Shop by Category{" "}
+                                        <span className="text-lg">{category ? "-" : "+"}</span>
+                                    </h1>
+                                    {category && (
+                                        <motion.ul
+                                            initial={{ y: 15, opacity: 0 }}
+                                            animate={{ y: 0, opacity: 1 }}
+                                            transition={{ duration: 0.4 }}
+                                            className="text-sm flex flex-col gap-1"
+                                        >
+                                            <li className="headerSedenavLi">New Arrivals</li>
+                                            <li className="headerSedenavLi">Gudgets</li>
+                                            <li className="headerSedenavLi">Accessories</li>
+                                            <li className="headerSedenavLi">Electronics</li>
+                                            <li className="headerSedenavLi">Others</li>
+                                        </motion.ul>
+                                    )}
+                                </div>
+                                <div className="mt-4">
+                                    <h1
+                                        onClick={() => setBrand(!brand)}
+                                        className="flex justify-between text-base cursor-pointer items-center font-titleFont mb-2"
+                                    >
+                                        Shop by Brand
+                                        <span className="text-lg">{brand ? "-" : "+"}</span>
+                                    </h1>
+                                    {brand && (
+                                        <motion.ul
+                                            initial={{ y: 15, opacity: 0 }}
+                                            animate={{ y: 0, opacity: 1 }}
+                                            transition={{ duration: 0.4 }}
+                                            className="text-sm flex flex-col gap-1"
+                                        >
+                                            <li className="headerSedenavLi">New Arrivals</li>
+                                            <li className="headerSedenavLi">Gudgets</li>
+                                            <li className="headerSedenavLi">Accessories</li>
+                                            <li className="headerSedenavLi">Electronics</li>
+                                            <li className="headerSedenavLi">Others</li>
+                                        </motion.ul>
+                                    )}
+                                </div>
+                            </div>
+                            <span
+                                onClick={() => setSidenav(false)}
+                                className="w-8 h-8 border-[1px] border-gray-300 absolute top-2 -right-10 text-gray-300 text-2xl flex justify-center items-center cursor-pointer hover:border-red-500 hover:text-red-500 duration-300"
+                            >
+                                <MdClose />
+                            </span>
+                        </motion.div>
+                    </div>
+                )}
             </nav>
         </div>
     );
