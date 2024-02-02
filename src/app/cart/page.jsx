66f4a99f7ab9cce24459ccb2cf9c7268596c89c6
ashik-ahmed <ashikahmed121@gmail.com>
2024-02-02 23:36:41 +0600

    cart page metadata updated

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,6 +1,10 @@
 import Cart from '@/components/Cart'
 import React from 'react'
 
+export const metadata = {
+    title: 'Asago Fashion | Cart',
+    description: 'One stop solution for all your fashion needs.',
+}
 const CartPage = () => {
 
     return (
