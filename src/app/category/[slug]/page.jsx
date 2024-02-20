fc01e816dd9b0edcf5ea4f58026e7cee281e1ce9
ashik-ahmed <ashikahmed121@gmail.com>
2024-02-20 15:38:00 +0600

    dunamic category page created

---
+++ src/app/category/[slug]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoryPage = () => {
+    return (
+        <div>
+            Hello from category page.....
+        </div>
+    );
+};
+
+export default CategoryPage;
